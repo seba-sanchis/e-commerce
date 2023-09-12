557c2b7e861cd8e4b634c2cf35eb0a56a96cdc36
seba-sanchis <seba.sanchis@gmail.com>
2023-09-11 23:50:50 -0300

    nextauth

--- models/user.ts
+++ models/user.ts
@@ -3,7 +3,7 @@ import { Schema, model, models } from "mongoose";
 const UserSchema = new Schema({
   name: {
     type: String,
-    required: [true, "Name is required!"],
+    required: [false, "Name is required!"],
     match: [
       /^(?=.{6,32}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._\s]+(?<![_.])$/,
       "Name invalid, it should contain 6-32 alphanumeric letters!",
@@ -21,7 +21,7 @@ const UserSchema = new Schema({
   email: {
     type: String,
     unique: [true, "Email already exists!"],
-    required: [true, "Email is required and it should be unique!"],
+    required: [false, "Email is required and it should be unique!"],
   },
   image: String,
   areaCode: Number,
