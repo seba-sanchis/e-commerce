e4f5598583769757a7a748a63f5362171a421546
seba-sanchis <seba.sanchis@gmail.com>
2023-09-17 17:25:37 -0300

    testing

--- app/api/payment/route.ts
+++ app/api/payment/route.ts
@@ -10,68 +10,62 @@ export const POST = async (request: NextRequest) => {
   try {
     if (type === "payment") {
       console.log("1st step (data):", data.id);
+      const { body } = await mercadopago.payment.findById(data.id);
+      console.log("2nd step:", body);
 
-      try {
-        const { body } = await mercadopago.payment.findById(data.id);
-        console.log("2nd step:", body);
+      const order = {
+        orderId: body.order.id,
+        date: body.date_approved,
+        status: body.status,
+        installments: body.installments,
+        reference: body.external_reference,
+      };
 
-        const order = {
-          orderId: body.order.id,
-          date: body.date_approved,
-          status: body.status,
-          installments: body.installments,
-          reference: body.external_reference,
-        };
+      const payer = {
+        firstName: body.payer.first_name,
+        lastName: body.payer.last_name,
+        email: body.payer.email,
+        identification: body.payer.identification.number,
+      };
 
-        const payer = {
-          firstName: body.payer.first_name,
-          lastName: body.payer.last_name,
-          email: body.payer.email,
-          identification: body.payer.identification.number,
-        };
+      const payment = {
+        company: body.payment_method.id,
+        type: body.payment_method.type,
+      };
 
-        const payment = {
-          company: body.payment_method.id,
-          type: body.payment_method.type,
-        };
+      const phone = {
+        areaCode: body.payer.phone.area_code,
+        number: body.payer.phone.number,
+        extension: body.payer.phone.extension,
+      };
 
-        const phone = {
-          areaCode: body.payer.phone.area_code,
-          number: body.payer.phone.number,
-          extension: body.payer.phone.extension,
-        };
+      const picked = body.additional_info.items.map((item: PreferenceItem) => ({
+        category: item.category_id,
+        description: item.description,
+        sku: item.id,
+        thumbnail: item.picture_url,
+        quantity: item.quantity,
+        name: item.title,
+        price: item.unit_price,
+      }));
 
-        const picked = body.additional_info.items.map(
-          (item: PreferenceItem) => ({
-            category: item.category_id,
-            description: item.description,
-            sku: item.id,
-            thumbnail: item.picture_url,
-            quantity: item.quantity,
-            name: item.title,
-            price: item.unit_price,
-          })
-        );
+      const transaction = {
+        bank: body.transaction_details.financial_institution,
+        installment: body.transaction_details.installment_amount,
+        paid: body.transaction_details.total_paid_amount,
+        received: body.transaction_details.net_received_amount,
+        overpaid: body.transaction_details.overpaid_amount,
+      };
 
-        const transaction = {
-          bank: body.transaction_details.financial_institution,
-          installment: body.transaction_details.installment_amount,
-          paid: body.transaction_details.total_paid_amount,
-          received: body.transaction_details.net_received_amount,
-          overpaid: body.transaction_details.overpaid_amount,
-        };
-
-        await newOrder(order, payer, payment, phone, picked, transaction);
-      } catch (error) {
-        console.error("Error fetching payment details:", error);
-        // Handle the error, log it, and return an appropriate response
-      }
+      await newOrder(order, payer, payment, phone, picked, transaction);
     }
 
     return NextResponse.json({ status: 201 });
-  } catch (error) {
+  } catch (error: any) {
+    console.log("Failed to create a new order: ", error.message);
+
     return NextResponse.json(
-      { message: "Failed to create a new customer" },
+      { message: "Failed to create a new order" },
       { status: 500 }
     );
   }
