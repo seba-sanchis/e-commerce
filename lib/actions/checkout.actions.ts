d832af68c386381b711306064da87e2a378331c2
seba-sanchis <seba.sanchis@gmail.com>
2023-09-19 12:20:31 -0300

    production checkout

--- lib/actions/checkout.actions.ts
+++ lib/actions/checkout.actions.ts
@@ -21,9 +21,9 @@ export async function newCheckOut(
 ) {
   // Add credentials
   mercadopago.configure({
-    access_token: MERCADOPAGO_ACCESS_TOKEN!,
-    client_id: MERCADOPAGO_CLIENT_ID,
-    client_secret: MERCADOPAGO_CLIENT_SECRET,
+    // access_token: MERCADOPAGO_ACCESS_TOKEN!,
+    client_id: MERCADOPAGO_CLIENT_ID!,
+    client_secret: MERCADOPAGO_CLIENT_SECRET!,
   });
 
   try {
