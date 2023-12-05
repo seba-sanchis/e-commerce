8f46c4337834896688f0d8b8f18f25f1d8fc2c5a
seba-sanchis <seba.sanchis@gmail.com>
2023-12-05 14:28:55 -0300

    migrating to mercadopago 2.0

--- app/api/payment/route.ts
+++ app/api/payment/route.ts
@@ -1,7 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
-import { PreferenceItem } from "mercadopago/models/preferences/create-payload.model";
+
 
 import { newOrder } from "@/lib/actions/order.actions";
+import { MercadoPagoConfig, Payment } from 'mercadopago';
 
 const { MERCADOPAGO_ACCESS_TOKEN } = process.env;
 
@@ -12,17 +13,23 @@ export const POST = async (request: NextRequest) => {
     if (type === "payment") {
       console.log("data.id ->", data.id);
 
-      const response = await fetch(
-        `https://api.mercadopago.com/v1/payments/${data.id}`,
-        {
-          headers: {
-            "Content-type": "application/json",
-            Authorization: `Bearer ${MERCADOPAGO_ACCESS_TOKEN}`,
-          },
-        }
-      );
+      const client = new MercadoPagoConfig({ accessToken: `${MERCADOPAGO_ACCESS_TOKEN}` });
+      
+      const payment = new Payment(client);
 
-      const body = await response.json();
+      const body = await payment.get(data.id);
+
+      // const response = await fetch(
+      //   `https://api.mercadopago.com/v1/payments/${data.id}`,
+      //   {
+      //     headers: {
+      //       "Content-type": "application/json",
+      //       Authorization: `Bearer ${MERCADOPAGO_ACCESS_TOKEN}`,
+      //     },
+      //   }
+      // );
+
+      // const body = await response.json();
 
       console.log("body ->", body);
 
