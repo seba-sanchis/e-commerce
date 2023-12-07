0a2ef3fcdd772ab5cb8cf0190ce8c042c3013409
seba-sanchis <seba.sanchis@gmail.com>
2023-12-07 11:32:41 -0300

    trusted domain

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["lh3.googleusercontent.com", "mlstatic.com"],
+    domains: ["lh3.googleusercontent.com", "http2.mlstatic.com"],
   },
 };
 
