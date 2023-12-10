644d6e207dd52fccfd7290e10d8285340e0d45fd
seba-sanchis <seba.sanchis@gmail.com>
2023-12-10 20:51:03 -0300

    allow any origin

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -13,7 +13,7 @@ export const GET = async () => {
   try {
     // Set CORS headers
     const headers = {
-      "Access-Control-Allow-Origin": "https://ss-dashboard-angular.vercel.app",
+      "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
       "Access-Control-Allow-Headers": "Content-Type, Authorization",
     };
