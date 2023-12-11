b73ae1f58cf98e28dc1c5c9d2450ba23cee177be
seba-sanchis <seba.sanchis@gmail.com>
2023-12-10 22:04:38 -0300

    cors headers

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -13,7 +13,7 @@ export const GET = async () => {
   try {
     // Set CORS headers
     const headers = {
-      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Origin": "https://65765e94bb457509ca524d82--cozy-sunburst-022120.netlify.app",
       "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
       "Access-Control-Allow-Headers": "Content-Type, Authorization",
     };
